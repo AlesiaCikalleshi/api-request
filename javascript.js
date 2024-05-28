bab1fcc03b1c085e8eb1709a10d58856a24aa2d8
AlesiaCikalleshi <118731784+AlesiaCikalleshi@users.noreply.github.com>
2024-05-28 21:19:56 +0200

    promise and await features added

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-function getRequests() {
+async function getRequests() {
     const urls = [
         'https://api.artic.edu/api/v1/artworks/129883',
         'https://api.artic.edu/api/v1/artworks/129884',
@@ -6,37 +6,34 @@ function getRequests() {
     ];
 
     const cardContainer = document.getElementById('cards-container');
-    cardContainer.innerHTML = ''; // Clear existing content
+    cardContainer.innerHTML = '';
 
+    try {
+        const fetchPromises = urls.map(url => fetch(url).then(response => response.json()));
+        const results = await Promise.all(fetchPromises);
 
-    urls.forEach(url => {
-        fetch(url)
-            .then(response => response.json())
-            .then(data => {
-                const title = data.data.title;
-                const artist = data.data.artist_display;
-                const artwork = data.data.artwork_type_title;
-                const categoryTitle = data.data.category_titles;
-                const link = data.data.api_link;
+        results.forEach(data => {
+            const title = data.data.title;
+            const artist = data.data.artist_display;
+            const artwork = data.data.artwork_type_title;
+            const categoryTitle = data.data.category_titles;
+            const link = data.data.api_link;
 
-                const card = createCard(categoryTitle, title, artist, artwork, link)
-                cardContainer.appendChild(card)
-            })
-            .catch(error => console.log(error))
-    })
-
-
-
-
+            const card = createCard(categoryTitle, title, artist, artwork, link);
+            cardContainer.appendChild(card);
+        });
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 
 function createCard(categoryTitle, title, artist, artwork, link) {
     const card = document.createElement('div');
-    card.className = 'card'
+    card.className = 'card';
 
     const container = document.createElement('div');
-    container.className = 'container'
+    container.className = 'container';
 
     const h4 = document.createElement('h4')    
     const b = document.createElement('b')
